0f5b3c2b08b5509b3b43235921151d3237da4475
Lora Titorenkova <lora.titorenkova@gmail.com>
2017-07-28 11:11:23 +0300

    First commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var name = "Lora Titorenkova";
+var role = "Junior front-end web develeoper";
+
+var formattedName = HTMLheaderName.replace("%data%",name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
